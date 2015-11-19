eb38dd2a32a3eeea54d2c4ce79cad5025f67ebc2
Paul-Hess <pauljacksonhess@gmail.com>
2015-11-18 19:06:41 -0800

    create user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase! }
-	validates :name, presence: true, length: { maximum: 50 }
-	validates :email, presence: true, length: { maximum: 255 }, 
-	format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i },
-	uniqueness: { case_sensitive: false }
+	before_save { email.downcase! }
+	validates :name, presence: true, 
+									 length: { maximum: 50 }
+
+	validates :email, presence: true,
+										length: { maximum: 255 },
+										format: { with: /\A[\w+\-.]+@[a-z0-9\-.]+\.[a-z]+\z/i },
+										uniqueness: { case_sensitive: false }
 	has_secure_password
-	validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*[\W])(?=.*[0-9]).{6,}\z/ }
+	
+	validates :password, presence: true, 
+											 length: { minimum: 6 },
+											 format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*[\W])(?=.*[0-9]).{6,}\z/ }
 
 end
+
