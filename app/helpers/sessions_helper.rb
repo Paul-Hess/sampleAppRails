2ce5e818f49474d7894f125786e16200c92ca750
Paul-Hess <pauljacksonhess@gmail.com>
2015-11-24 18:55:41 -0800

    friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,11 +3,15 @@ module SessionsHelper
 	def log_in(user)
 		session[:user_id] = user.id
 	end
-
+	# Remembers a user in a permission session.
 	def remember(user)
 		user.remember
 		cookies.permanent.signed[:user_id] = user.id
 		cookies.permanent[:remember_token] = user.remember_token
+	end
+
+	def current_user?(user)
+		user == current_user
 	end
 	def current_user
 		if  (user_id = session[:user_id])
@@ -37,4 +41,14 @@ module SessionsHelper
 		@current_user = nil
 	end
 	
+	# Redirects to stored location (or to the default)
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+
+	# Stored the URL trying to be accessed.
+	def store_location
+		session[:forwarding_url] = request.url if request.get?
+	end
 end
