3cfb8adfc8eb8917f7ce4e9c557939e630414450
Paul-Hess <pauljacksonhess@gmail.com>
2015-12-02 12:53:13 -0800

    add user microposts

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,12 @@
+if Rails.env.production?
+	CarrierWave.configure do |config|
+		config.fog_credentials = {
+
+			:provider 												=> "Google",
+			:google_storage_access_key_id			=> Rails.application.secrets.google_storage_access_key_id,
+			:google_storage_secret_access_key => Rails.application.secrets.google_storage_secret_access_key	
+	}
+
+	config.fog_directory = ENV["sample_app_bucket"]
+	end
+end
