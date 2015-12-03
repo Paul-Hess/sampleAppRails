b1fa01c0e146ec60974b7a072f55fc46aff2389d
Paul-Hess <pauljacksonhess@gmail.com>
2015-12-03 15:07:47 -0800

    fix error with heroku

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,10 +2,10 @@ if Rails.env.production?
 	CarrierWave.configure do |config|
 		config.fog_credentials = {
 
-			:provider 												=> "AWS",
-			:aws_access_key_id								=> ENV["AKIAIBL5HBIDATDW3JWQ"],
-			:aws_secret_access_key 						=> ENV["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"],
-			:region														=> ENV["us-west-2"]
+			provider: "AWS",
+			aws_access_key_id: ENV["S3_ACCESS_KEY"],
+			aws_secret_access_key: ENV["S3_SECRET_KEY"],
+			region: ENV["us-west-2"]
 	}
 
 	config.fog_directory = ENV["sampappaul"]
