160802bdaadc7b03451db4ea06ee47ec323f2d5f
Paul-Hess <pauljacksonhess@gmail.com>
2015-12-02 21:05:54 -0800

    add config for AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,11 @@ if Rails.env.production?
 	CarrierWave.configure do |config|
 		config.fog_credentials = {
 
-			:provider 												=> "Google",
-			:google_storage_access_key_id			=> ENV["GOOGSGTWYQKTCW3RMR4Z"],
-			:google_storage_secret_access_key => ENV["HWSnLQEYecB3YUOpcm8k6CqkZZKK2Ms/Il7LZF9z"]
+			:provider 												=> "AWS",
+			:aws_access_key_id			=> Rails.application.secrets.aws_access_key_id,
+			:aws_secret_access_key => Rails.application.secrets.aws_secret_access_key
 	}
 
-	config.fog_directory = ENV["sample_app_bucket"]
+	config.fog_directory = ENV["sampappaul"]
 	end
 end
